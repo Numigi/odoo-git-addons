aaa02be20b0940a3c70208141077eba90bfb50c9
Quentin Lavallée-Bourdeau <quentin.lavalleeb@gmail.com>
2024-08-29 14:59:44 -0400

    TA# 68951 TA#68776 [ADD] numistore to v14

---
+++ numistore/README.rst
@@ -0,0 +1,27 @@
+Numistore
+=========
+This module sets up a place to discover and discuss odoo modules.
+
+It is based on the store developed by OCA.
+
+Settings
+--------
+
+Need to setup the `[options] github_token` in odoo.conf file.
+See https://github.com/settings/tokens to generate your GitHub token.
+
+Specifics
+---------
+Numigi doesn't use the store as it was thought initially by OCA. We want to use it
+as a knowledge base.
+
+To help to share and discuss, we then customize the solution with this purpose in mind.
+
+Chatter on odoo.module.version
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+.. image:: static/description/chatter_odoo_module_version.png
+
+Contributors
+------------
+* Numigi (tm) and all its contributors (https://bit.ly/numigiens)
---
+++ numistore/models/models.py
@@ -0,0 +1,38 @@
+# © 2023 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import models, fields, api
+
+organisation_default_series = (
+    '11.0',
+    '12.0',
+    '13.0',
+    '14.0',
+)
+
+
+class OdooModuleChatter(models.Model):
+    _name = "odoo.module"
+    _inherit = ['odoo.module', 'mail.thread', 'mail.activity.mixin']
+
+
+class GithubRepoBranch(models.Model):
+    _inherit = 'github.repository.branch'
+
+    # add the ./addons/ folder so it is also scanned in client repo
+    # TA#16291
+    module_paths = fields.Text(default="./\n./addons")
+
+
+class GithubOrganization(models.Model):
+    _inherit = 'github.organization'
+
+    @api.model
+    def create_from_name(self, name):
+        ret = super().create_from_name(name)
+        series_obj = self.env['github.organization.serie']
+        details = [
+            {'sequence': i, 'name': name, 'organization_id': ret.id}
+            for i, name in enumerate(organisation_default_series)
+        ]
+        series_obj.create(details)
