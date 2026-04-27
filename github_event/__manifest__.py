cf08b512d5e7b096d7960c73be16352b5edff8ef
Abdellatif Benzbiria <odoo-base@numigi.com>
2026-04-27 13:50:33 +0000

    [18.0][MIG] github_event

--- github_event/__manifest__.py
+++ github_event/__manifest__.py
@@ -11,7 +11,7 @@
     "category": "Connector",
     "summary": "Define what is a github event as an odoo object",
     "depends": [
-        "base_sparse_field",
+        "base",
     ],
     "data": [
         "security/ir.model.access.csv",
